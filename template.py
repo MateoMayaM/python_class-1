165222f3f02bb269641ead831db8607d99ad0614
Danigore25 <73318665+Danigore25@users.noreply.github.com>
2021-03-24 16:35:04 -0600

    Create template.py

---
+++ template.py
@@ -0,0 +1,28 @@
+'''
+NAME
+  [name]
+
+VERSION 
+  [#.#]
+
+AUTHOR
+  [Full name] <[username]@gmail.com>
+  [Other authors]: [Modifications]
+
+DESCRIPTION
+  [description]
+
+USAGE
+  [program name][options]
+
+ARGUMENTS
+  [name]  [description]
+
+INPUT
+  [files or directories used to run the program and formats]
+
+OUTPUT
+  [file names and formats]
+
+
+'''
