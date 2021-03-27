9593c3f64a142520da04502e8a40c47851f2e318
gorec <danigore22@gmail.com>
2021-03-26 20:19:04 -0600

    Se añadió archivo template.py

---
+++ templates/template.py
@@ -0,0 +1,32 @@
+'''
+NAME
+	[name]
+
+VERSION 
+	[#.#]
+	
+AUTHOR
+	[Full name] <[username]@gmail.com>
+	[Other authors]: [Modifications]
+
+DESCRIPTION
+	[description]
+
+CATEGORY
+	[category]
+
+USAGE
+	[program name][options]
+
+ARGUMENTS
+	[name]  [description]
+        [name]  [description]
+	[name]  [description]
+
+INPUT
+	[files or directories used to run the program and formats]
+
+OUTPUT
+	[file names and formats]
+
+'''
