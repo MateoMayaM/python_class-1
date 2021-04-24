1264db382df1b03163923c97af780025e879a16e
Danigore25 <73318665+Danigore25@users.noreply.github.com>
2021-04-24 14:33:46 -0500

    "Se comentó una parte del archivo de ejemplo Rosalind_ComplementaryDNA.py"

--- src/Examples/Rosalind_ComplementaryDNA.py
+++ src/Examples/Rosalind_ComplementaryDNA.py
@@ -1,7 +1,7 @@
 print("DNA sequence: ")
 sequence = input()
-dna = sequence[::-1]
-dna = dna.replace("A","t")
+dna = sequence[::-1]     # Se revierte la secuencia dada por el usuario
+dna = dna.replace("A", "t")
 dna = dna.replace("C", "g")
 dna = dna.replace("G", "c")
 dna = dna.replace("T", "a")
