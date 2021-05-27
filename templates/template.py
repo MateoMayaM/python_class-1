00c4ae29fea01641398b317b86f465ebdc535648
Danigore25 <73318665+Danigore25@users.noreply.github.com>
2021-05-26 20:23:56 -0500

    Se actualizó el archivo template.py

--- templates/template.py
+++ templates/template.py
@@ -19,9 +19,19 @@ USAGE
 	[program name][options]
 
 ARGUMENTS
-	[name]  [description]
-        [name]  [description]
-	[name]  [description]
+	[name]  [description]	[required/not required]
+        [name]  [description]	[required/not required]
+	[name]  [description]	[required/not required]
+
+PACKAGES
+	[name]	[usage]
+	[name]	[usage]
+	[name]	[usage]
+
+FUNCTIONS
+	[name]	[description]
+	[name]	[description]
+	[name]	[description]
 
 INPUT
 	[files or directories used to run the program and formats]
@@ -31,5 +41,8 @@ OUTPUT
 
 EXAMPLES
 	[Example 1: Describe the case, the input and output]
+	
+GITHUB LINK
+	<url>
 
 '''
