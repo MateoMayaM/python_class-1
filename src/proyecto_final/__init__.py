acee2ff0a3160b85c73b049b83e6b8ae6700ef72
Danigore25 <73318665+Danigore25@users.noreply.github.com>
2021-06-23 22:41:38 -0500

    "Se creó el paquete proyecto_final con los módulos modulo1.py, modulo2.py, modulo3.py, modulo4.py y el archivo __init__.py. Se creó el programa de ejecución del paquete modulos_final.py"

--- src/proyecto_final/__init__.py
+++ src/proyecto_final/__init__.py
@@ -1,3 +1,5 @@
+# Este es un archivo de iniciación del paquete proyecto_final.
+
 # 1. Invocar a cada una de las funciones pertenecientes a los módulos del paquete proyecto_final.
 
 from proyecto_final.modulo1 import get_gc_content
